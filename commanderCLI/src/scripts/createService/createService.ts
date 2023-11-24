efa11d61a800e765c575c09cd6519a6cea8e840b
Maksim Timofeev <maxf.f2014@yandex.ru>
2023-11-24 14:23:45 +0400

    use func for command

--- commanderCLI/src/scripts/createService/createService.ts
+++ commanderCLI/src/scripts/createService/createService.ts
@@ -1,26 +1,25 @@
 #!/usr/bin/env node
 
 import { Command } from "commander";
-import path from "node:path";
 import { Plop, run } from "plop";
-import { dirname } from "node:path";
+import { resolve, dirname } from "node:path";
 import { fileURLToPath } from "node:url";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
+export const createTemplate = () => {
+  Plop.prepare(
+    {
+      configPath: resolve(__dirname, "../../../plopfile.js"),
+    },
+    (env) => Plop.execute(env, run as any)
+  );
+};
+
 export function makeCreateService() {
   const service = new Command("createService")
     .description("Копирует шаблон услуги в директорию")
-    .action(() => {
-      console.log(__dirname);
-
-      Plop.prepare(
-        {
-          configPath: path.resolve(__dirname, "../../../plopfile.js"),
-        },
-        (env) => Plop.execute(env, run as any)
-      );
-    });
+    .action(createTemplate);
 
   return service;
 }
--- commanderCLI/src/scripts/example/exampleCommand.ts
+++ commanderCLI/src/scripts/example/exampleCommand.ts
@@ -5,14 +5,16 @@ import inquirer from "inquirer";
 import { PROMPTS } from "./prompts.js";
 import { Answers } from "./types.js";
 
+export const exampleFunction = () => {
+  inquirer.prompt(PROMPTS).then((answer: Answers) => {
+    console.log(answer.message);
+  });
+};
+
 export function makeExampleCommand() {
   const example = new Command("example")
-    .description("Test command for sending a message to console.log")
-    .action(() => {
-      inquirer.prompt(PROMPTS).then((answer: Answers) => {
-        console.log(answer.message);
-      });
-    });
+    .description("Тестовая команда для вывода любого сообщения в console.log")
+    .action(exampleFunction);
 
   return example;
 }
